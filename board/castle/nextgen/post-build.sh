8bd3c9273b44960dab320ada66b1595bc466f6c5
Andy R <andy@azureussolutions.com>
2026-09-21 22:49:33 +0100

    Create boot mount point in NextGen rootfs

--- board/castle/nextgen/post-build.sh
+++ board/castle/nextgen/post-build.sh
@@ -8,7 +8,7 @@ APP_DIR="${NEXTGEN_APP_DIR:-$WORKSPACE_DIR/app}"
 PRODUCT="${NEXTGEN_PRODUCT:-sound}"
 EXEC_DIR="$TARGET_DIR/root/Exec"
 COMMON_RUNTIME="$APP_DIR/Application/Files/Runtime/Sound/Exec"
-mkdir -p "$TARGET_DIR/root" "$EXEC_DIR"
+mkdir -p "$TARGET_DIR/root" "$TARGET_DIR/boot" "$EXEC_DIR"
 
 for name in Arial.ttf NotoSansCJKtc-Regular.ttf Translations.csv ionicons.ttf open-iconic.ttf; do
     [ ! -e "$COMMON_RUNTIME/$name" ] || cp -L "$COMMON_RUNTIME/$name" "$EXEC_DIR/$name"
