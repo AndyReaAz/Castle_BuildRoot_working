fe7ccbe50eafd9b1ae2598fa923f7c6189683906
Andy R <andy@azureussolutions.com>
2026-09-25 19:45:24 +0100

    Use ratified production MTD map in bring-up

--- board/castle/nextgen/nextgen-bringup.sh
+++ board/castle/nextgen/nextgen-bringup.sh
@@ -46,7 +46,7 @@ main()
 
     [ -r /proc/mtd ] || fail "/proc/mtd is unavailable" || return 1
 
-    for label in at91bootstrap uboot uboot-env device-tree kernel rootfs
+    for label in at91bootstrap uboot uboot-env boot rootfs
     do
         require_mtd_label "$label" || return 1
     done
